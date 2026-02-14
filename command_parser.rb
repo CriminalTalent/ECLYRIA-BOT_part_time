923c4b06b6bd32c15f781ca71e99da4d4e1e0628
CriminalTalent <rntjdrud@naver.com>
2026-02-14 17:51:54 +0900

    Update command_parser.rb

--- command_parser.rb
+++ command_parser.rb
@@ -1,52 +1,37 @@
 # command_parser.rb
-require_relative 'commands/job_list_command'
+# encoding: UTF-8
+require 'cgi'
 require_relative 'commands/job_start_command'
-require_relative 'commands/wallet_command'
 
-class CommandParser
-  def initialize(mastodon_client, sheet_manager)
-    @mastodon_client = mastodon_client
-    @sheet_manager = sheet_manager
+module CommandParser
+  def self.parse(client, sheet_manager, notification)
+    content_raw = notification.dig("status", "content") || ""
+    sender = notification.dig("account", "acct") || ""
     
-    @job_list_command = JobListCommand.new(mastodon_client, sheet_manager)
-    @job_start_command = JobStartCommand.new(mastodon_client, sheet_manager)
-    @wallet_command = WalletCommand.new(mastodon_client, sheet_manager)
-  end
-
-  def parse(notification)
-    status = notification['status']
-    return unless status
-    
-    account = notification['account']
-    sender = account['acct'].split('@').first
-    
-    content = clean_html(status['content'])
+    content = clean_html(content_raw)
     
     puts "[파서] @#{sender}: #{content}"
-    
+
     case content
-    when /\[알바목록\]/i
-      @job_list_command.execute(sender, status)
-      
-    when /\[알바시작\/(.+?)\]/i
+    when /\[알바시작\/(.+?)\]/
       job_name = $1.strip
-      @job_start_command.execute(sender, job_name, status)
-      
-    when /\[지갑\]/i
-      @wallet_command.execute(sender, status)
-      
+      JobStartCommand.new(client, sheet_manager, notification, sender, job_name).execute
     else
-      puts "[무시] 알 수 없는 명령어"
+      puts "[파서] 인식 불가"
     end
-    
+
   rescue => e
     puts "[에러] 명령어 처리 실패: #{e.message}"
-    puts e.backtrace.first(3)
+    puts e.backtrace.first(5).join("\n")
   end
 
-  private
-
-  def clean_html(html)
-    html.gsub(/<[^>]*>/, '').strip
+  def self.clean_html(html)
+    return "" if html.nil?
+    s = html.gsub(/<br\s*\/?>/i, "\n")
+           .gsub(/<\/p>/i, "\n")
+           .gsub(/<[^>]*>/, "")
+    CGI.unescapeHTML(s).gsub("\u00A0", " ").strip
+  rescue
+    html.to_s
   end
 end
